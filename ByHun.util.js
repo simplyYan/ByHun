6b7ef833e9e82cd6e121ea6a7dbe50b30def4040
Wesley Yan S. B. <102262759+simplyYan@users.noreply.github.com>
2026-01-13 14:53:15 +0000

    Change 'AppID' label to 'Source' in modal

--- ByHun.util.js
+++ ByHun.util.js
@@ -114,7 +114,7 @@ const ByHun = (function () {
       <h2>${currentApp.name}</h2>
       <p><strong>Developer:</strong> ${currentApp.dev}</p>
       <p><strong>AppID:</strong> ${currentApp.appID}</p>
-      <p><strong>AppID:</strong> ${currentApp.source}</p>
+      <p><strong>Source:</strong> ${currentApp.source}</p>
       <button onclick="window.open('https://byhun.gt.tc', '_blank')">Download ByHun</button>
       <button class="byhun-modal-close">Close</button>
     `;
